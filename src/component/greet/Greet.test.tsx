a1943bfa3a80712348b57dd6763ed62f602971cf
Vishal k <vkulkarni149@gmail.com>
2023-06-27 11:02:02 +0530

    1) Greet should render the text hello and if a name is passed into the component 2) it should render hello followed by name

--- src/component/greet/Greet.test.tsx
+++ src/component/greet/Greet.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react'
 import { Greet } from './Greet'
-
+/*
+Here is TDD approach example we following , lets see the requirements
+1) Greet should render the text hello and if a name is passed into the component 2) it should render hello followed by name 
+*/
 test('Greet renders correctly', () => {
   render(<Greet />)
-  const textElement = screen.getByText(/hello/i)
+  const textElement = screen.getByText('Hello')
+  expect(textElement).toBeInTheDocument()
+})
+
+test('Greet renders the name', () => {
+  render(<Greet name='vishal' />)
+  const textElement = screen.getByText('Hello vishal')
   expect(textElement).toBeInTheDocument()
 })
