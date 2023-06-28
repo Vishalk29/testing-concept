b0118527c7332dd6c9edb3fbaa0dfadd763461e1
Vishal k <vkulkarni149@gmail.com>
2023-06-28 11:48:50 +0530

    learned about getByText

--- src/component/application/Application.test.tsx
+++ src/component/application/Application.test.tsx
@@ -13,6 +13,10 @@ describe('Application', () => {
       level: 2,
     })
     expect(sectionHeading).toBeInTheDocument()
+
+    const paragraphElement = screen.getByText('All fields are mandatory')
+    expect(paragraphElement).toBeInTheDocument()
+
     const nameElement = screen.getByRole('textbox', {
       name: 'Name',
     })
